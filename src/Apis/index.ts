bf1ef877d08a8a352142ed9914e66e994bb3554a
Ducnt28 <nguyentrungduc2807@gmail.com>
2024-08-27 16:59:51 +0700

    67

--- src/Apis/index.ts
+++ src/Apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instace = axios.create({
-  baseURL: 'http://localhost:2000',
+  baseURL: 'http://localhost:2000234234234',
   headers: {
     'Content-Type': 'application/json'
   }
